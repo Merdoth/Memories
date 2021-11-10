66253fb1ffdaa94d5fa003ce1e2e48ad7c31b068
Merdoth <ucheya97@gmail.com>
2021-11-10 13:54:31 +0100

    initial post features setup Add create post features

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ import postsRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postsRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://Merdoth:merdoth1@cluster0.6b85q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.use('/posts', postsRoutes);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.6b85q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
